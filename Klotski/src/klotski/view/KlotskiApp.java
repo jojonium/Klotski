4f702be7f127fd3362b403e1bcda8f827ad072cf
Joseph Petitti <jojonium@autoreg-072019.dyn.wpi.edu>
2018-11-05 00:00:58 -0500

    Added controller hooks

--- Klotski/src/klotski/view/KlotskiApp.java
+++ Klotski/src/klotski/view/KlotskiApp.java
@@ -2,13 +2,13 @@ package klotski.view;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 
 import klotski.model.Board;
