ff04415f518091ebd680b3c1163e61f9e34cce0a
Joseph Petitti <jojonium@autoreg-072019.dyn.wpi.edu>
2018-11-07 17:14:43 -0500

    Added puzzle view tests

---
+++ Klotski/test/klotski/view/TestPuzzleView.java
@@ -0,0 +1,30 @@
+package klotski.view;
+
+import static org.junit.Assert.*;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+
+import org.junit.Test;
+
+import klotski.model.Board;
+
+public class TestPuzzleView {
+
+	@Test
+	public void testGetSquareSize() {
+		Board b = new Board();
+		PuzzleView pv = new PuzzleView(b);
+		assertEquals(100, pv.getSquareSize());
+	}
+
+	@Test
+	public void testGetPreferredSize() {
+		Board b = new Board();
+		PuzzleView pv = new PuzzleView(b);
+		Dimension dim = pv.getPreferredSize();
+		assertEquals(400, dim.width);
+		assertEquals(506, dim.height);
+	}
+}
