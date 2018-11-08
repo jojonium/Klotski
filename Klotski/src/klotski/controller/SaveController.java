14ca3e52d900a4c32765cab8867a44e16a22f209
Joseph Petitti <jojonium@autoreg-072019.dyn.wpi.edu>
2018-11-08 13:33:23 -0500

    Added save controller

---
+++ Klotski/src/klotski/controller/SaveController.java
@@ -0,0 +1,39 @@
+package klotski.controller;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import klotski.model.Board;
+
+public class SaveController {
+	final Board b;
+	final Path p;
+	
+	/**
+	 * Basic constructor
+	 * @param app the view application
+	 * @param b the model board
+	 */
+	public SaveController(Board b, Path p) {
+		this.b = b;
+		this.p = p;
+	}
+	
+	public boolean save() {
+		// Convert the string to a byte array.
+	    String s = b.toString();
+	    byte data[] = s.getBytes();
+
+	    try (OutputStream out = new BufferedOutputStream(
+	    		Files.newOutputStream(p))) {
+	      out.write(data, 0, data.length);
+	    } catch (IOException e) {
+	      System.err.println(e);
+	      return false;
+	    }
+	    return true;
+	}
+}
