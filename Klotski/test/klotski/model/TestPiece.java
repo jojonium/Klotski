b780f1eba539db0a9580319a7d6baa139236c129
Joseph Petitti <jojonium@autoreg-072019.dyn.wpi.edu>
2018-11-08 12:38:23 -0500

    Switched Piece class internals

--- Klotski/test/klotski/model/TestPiece.java
+++ Klotski/test/klotski/model/TestPiece.java
@@ -3,20 +3,14 @@ package klotski.model;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-import java.awt.Point;
-
 public class TestPiece {
 
 	@Test
 	public void testPiece() {
-		Point[] emptyPointArray = new Point[0];
-		Point[] onePoint = new Point[] {new Point(0, 0)};
-		Point[] manyPoints = new Point[] {new Point(1, 0), new Point(2, 0),
-				new Point(1, 1), new Point(2, 1)};
 		try {
-			new Piece(emptyPointArray);
-			new Piece(onePoint);
-			new Piece(manyPoints);
+			new Piece(0, 0, 0, 0);
+			new Piece(0, 0, 1, 1);
+			new Piece(3, 4, 8, 5);
 		} catch (Exception e) {
 			fail(e.getMessage());
 		}
@@ -24,16 +18,10 @@ public class TestPiece {
 
 	@Test
 	public void testMove() {
-		Point[] manyPoints = new Point[] {new Point(1, 1), new Point(2, 1),
-				new Point(1, 2), new Point(2, 2)};
-		Point[] onePoint = new Point[] {new Point(2, 2)};
-		Point[] twoPoints = new Point[] {new Point(1, 1), new Point(2, 1)};
-		Point[] threePoints = new Point[] {new Point(1, 1), new Point(1, 2),
-				new Point(1, 3)};
-		Piece testPieceA = new Piece(manyPoints);
-		Piece testPieceB = new Piece(onePoint);
-		Piece testPieceC = new Piece(twoPoints);
-		Piece testPieceD = new Piece(threePoints);
+		Piece testPieceA = new Piece(1, 1, 2, 2);
+		Piece testPieceB = new Piece(2, 2, 1, 1);
+		Piece testPieceC = new Piece(1, 1, 2, 1);
+		Piece testPieceD = new Piece(1, 1, 1, 3);
 		
 		try {
 			testPieceA.move(0); // move up
@@ -71,16 +59,13 @@ public class TestPiece {
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testMoveException() {
-		Point[] onePoint = new Point[] {new Point(2, 2)};
-		Piece testPieceB = new Piece(onePoint);
+		Piece testPieceB = new Piece(2, 2, 1, 1);
 		testPieceB.move(4); // this should fail
 	}
 
 	@Test
 	public void testContainsPoint() {
-		Point[] manyPoints = new Point[] {new Point(1, 1), new Point(2, 1),
-				new Point(1, 2), new Point(2, 2)};
-		Piece testPiece = new Piece(manyPoints);
+		Piece testPiece = new Piece(1, 1, 2, 2);
 		assertTrue(testPiece.containsPoint(1, 1));
 		assertTrue(testPiece.containsPoint(2, 1));
 		assertTrue(testPiece.containsPoint(1, 2));
@@ -91,14 +76,11 @@ public class TestPiece {
 
 	@Test
 	public void testGetDims() {
-		// points listed from bottom right to top left
-		Point[] backwardsPoints = new Point[] {new Point(3, 3), new Point(2, 3),
-				new Point(3, 2), new Point(2, 2)};
-		Piece testPiece = new Piece(backwardsPoints);
+		Piece testPiece = new Piece(8, 5, 1, 2);
 		int[] dimensions = testPiece.getDims();
-		assertEquals(dimensions[0], 2);
-		assertEquals(dimensions[1], 2);
-		assertEquals(dimensions[2], 2);
+		assertEquals(dimensions[0], 8);
+		assertEquals(dimensions[1], 5);
+		assertEquals(dimensions[2], 1);
 		assertEquals(dimensions[3], 2);
 	}
 
